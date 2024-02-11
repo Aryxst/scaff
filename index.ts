89f2b1ecc7c2a30f07e1ed4b7504e77f9cfe6470
Jessie Odeh <jessieodeh08@gmail.com>
2024-02-11 12:15:50 +0100

    feat: recognize if the template is file and then put package.json

--- index.ts
+++ index.ts
@@ -2,9 +2,8 @@ import { select, input, confirm } from '@inquirer/prompts';
 import { $ } from 'bun';
 import chalk from 'chalk';
 import { mappings } from './config';
-
+const templateDir = `${import.meta.dir}/templates`;
 const pwd = import.meta.env.PWD;
-const templateDir = `${pwd}/templates`;
 // Prompt the user to select a template to scaffold
 const template = await select({
  message: `Select a template to scaffold:`,
@@ -30,13 +29,6 @@ if (Number(await $`test -e ${pwd}/${name} && echo 1 || echo 0`.text())) {
  process.exit();
 }
 await $`cp ${templateDir}/${template} ./${name} -r`;
-// Check if the package already exists
-Number(await $`test -e ${templateDir}/${template}/package.json && echo 1 || echo 0`.text())
- ? // If the package exists, use sed to replace the "name" field in package.json
-   await $`sed -i 's/"name": .*/"name": "${name}"/' ./${name}/package.json'`
- : // If the package doesn't exist, ask the user if they want to create a new package.json
-   (await confirm({ message: 'No package.json found. Create one?' })) &&
-   // If the user confirms, create a new package.json with the specified "name"
-   (await $`echo '{"name": "${name}"}' > package.json`.cwd(name));
 
+Number(await $`test -e ${templateDir}/${template}/package.json && echo 1 || echo 0`.text()) ? await $`sed -i 's/"name": .*/"name": "${name}"/' ./${name}/package.json'` : !Number(await $`test -e ${templateDir}/${template}/ && echo 1 || echo 0`.text()) ? null : (await confirm({ message: 'No package.json found. Create one?' })) && (await $`echo '{"name": "${name}"}' > package.json`.cwd(name));
 console.log(`\n${chalk.green('Success!')} Created ${name} at ${pwd}/${name}\n\r`);
